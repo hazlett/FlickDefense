657aff7cc3d51ef9c56fdcf53ee8bb48704de254
bbricker87 <bbricker09@gmail.com>
2014-10-06 18:28:55 -0400

    Start of GameManager

---
+++ FlickDefense/Assets/Scripts/UpgradeSystem.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeSystem : MonoBehaviour {
+
+    private UpgradeSystem instance;
+    public UpgradeSystem Instance { get { return instance; } set { instance = value; } }
+
+    internal enum CastleLevel
+    {
+        LEVEL1,
+        LEVEL2,
+        LEVEL3,
+        LEVEL4,
+        LEVEL5
+    }
+
+    internal CastleLevel currentCastleLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
