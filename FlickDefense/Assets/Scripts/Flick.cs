983f2d6d2ad1d6983c857fb196e18f3975398931
hazlett <hazlett.206@gmail.com>
2014-10-06 18:27:38 -0400

    Flick behaviour

---
+++ FlickDefense/Assets/Scripts/Flick.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flick : MonoBehaviour {
+
+    public FlickBehaviour behaviour;
+    private Vector2 velocity;
+    private Vector3 screenToWorld, touchToWorld;
+    void Start () {
+	    
+	}
+	
+    void OnEnable()
+    {
+
+    }
+
+	void Update () {
+	}
+
+    public void SetPosition(Vector2 position)
+    {
+        rigidbody.useGravity = false;
+        touchToWorld = position;
+        touchToWorld.z = transform.position.z + Camera.main.nearClipPlane;
+        screenToWorld = Camera.main.ScreenToWorldPoint(touchToWorld);
+        transform.position = screenToWorld;
+    }
+
+    public void SetVelocity(Vector2 velocity)
+    {
+        this.velocity = velocity;
+        rigidbody.AddForce(velocity);
+        rigidbody.useGravity = true;
+        this.enabled = true;
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width * 0.5f, Screen.height * 0.5f, 500.0f, 50.0f), "FLICKED: " + velocity);
+        GUI.Box(new Rect(Screen.width * 0.5f, Screen.height * 0.5f + 75.0f, 500.0f, 50.0f), "Touch to world: (" + touchToWorld.x + ", " + touchToWorld.y + ", " + touchToWorld.z + ")");
+        GUI.Box(new Rect(Screen.width * 0.5f, Screen.height * 0.5f + 150.0f, 500.0f, 50.0f), "Screen to world: (" + screenToWorld.x + ", " + screenToWorld.y + ", " + screenToWorld.z + ")");
+    }
+}
---
+++ FlickDefense/Assets/Scripts/InputBehaviour.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputBehaviour : MonoBehaviour
+{
+
+    Touch userTouch;
+
+    private Vector2 startPos, movePos, endPos, velocity;
+    private bool hit;
+    private float deltaTime;
+    private Ray ray;
+    private RaycastHit raycastHit;
+
+    void Start()
+    {
+        hit = false;
+        deltaTime = 0.0000001f;
+        raycastHit = new RaycastHit();
+    }
+
+    void Update()
+    {
+
+        if (hit)
+        {
+            deltaTime += Time.deltaTime;
+        }
+
+        if (Input.touchCount > 0)
+        {
+            userTouch = Input.GetTouch(0);
+
+            switch (userTouch.phase)
+            {
+                case TouchPhase.Began:
+                    {
+                        startPos = userTouch.position;
+                        ray = Camera.main.ScreenPointToRay(startPos);
+                        hit = Physics.Raycast(ray, out raycastHit, 100.0f);
+                    }
+                    break;
+                default:
+                case TouchPhase.Moved:
+                    {
+                        if (hit)
+                        {
+                            try
+                            {
+                                raycastHit.collider.GetComponent<Flick>().SetPosition(userTouch.position);
+                            }
+                            catch (Exception e)
+                            {
+                                Debug.LogError("ERROR in TouchPhase.Moved: " + e.Message);
+                            }
+                        }
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    {
+                        if (hit)
+                        {
+                            endPos = userTouch.position;
+                            velocity.x = userTouch.deltaPosition.x / deltaTime;
+                            velocity.y = userTouch.deltaPosition.y / deltaTime;
+                            raycastHit.collider.GetComponent<Flick>().SetVelocity(velocity);
+                        }
+                        deltaTime = 0.0000001f;
+                        hit = false;
+                    }
+                    break;
+
+            }
+
+
+            //if (userTouch.phase == TouchPhase.Began)
+            //{
+            //    Debug.Log("BEGAN");
+            //    startPos = userTouch.position;
+            //    Ray ray = Camera.main.ScreenPointToRay(startPos);
+            //    RaycastHit raycastHit = new RaycastHit();
+            //    hit = Physics.Raycast(ray, out raycastHit, 100.0f);
+            //    if (hit)
+            //    {
+            //        Debug.Log("RAYCAST HIT: " + raycastHit.collider.name);
+            //    }
+            //}
+            //if (userTouch.phase == TouchPhase.Moved)
+            //{
+            //    movePos = userTouch.position;
+            //}
+            //if (userTouch.phase == TouchPhase.Ended)
+            //{
+            //    endPos = userTouch.position;
+            //    velocity.x = userTouch.deltaPosition.x / userTouch.deltaTime;
+            //    velocity.y = userTouch.deltaPosition.y / userTouch.deltaTime;
+            //}
+
+        }
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Box("Start Pos: (" + startPos.x + ", " + startPos.y + ")");
+        GUILayout.Box("Current Pos: (" + movePos.x + ", " + movePos.y + ")");
+        GUILayout.Box("End Pos: (" + endPos.x + ", " + endPos.y + ")");
+        GUILayout.Box("Velocity: (" + velocity.x + ", " + velocity.y + ")");
+    }
+}
