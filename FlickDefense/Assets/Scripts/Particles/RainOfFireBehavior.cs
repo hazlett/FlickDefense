180e621a2215142bba20d058a5a84e61688af727
bbricker87 <bbricker09@gmail.com>
2014-10-15 14:57:27 -0400

    JMO Assets

---
+++ FlickDefense/Assets/Scripts/Particles/RainOfFireBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RainOfFireBehavior : MonoBehaviour
+{
+    private ParticleSystem.CollisionEvent[] collisionEvents = new ParticleSystem.CollisionEvent[16];
+    void OnParticleCollision(GameObject other)
+    {
+        int safeLength = particleSystem.safeCollisionEventSize;
+        if (collisionEvents.Length < safeLength)
+            collisionEvents = new ParticleSystem.CollisionEvent[safeLength];
+
+        int numCollisionEvents = particleSystem.GetCollisionEvents(other, collisionEvents);
+        int i = 0;
+        while (i < numCollisionEvents)
+        {
+            GameObject explosion = (GameObject)GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/Particles/BomberExplosion"));
+            explosion.transform.position = collisionEvents[i].intersection;
+            i++;
+        }
+    }
+}
---
+++ FlickDefense/Assets/Scripts/Skills/FireSkill.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireSkill : MonoBehaviour {
+
+    private FireSkill instance;
+    public FireSkill Instance { get { return instance; } set { instance = value; } }
+
+    public GameObject rainOfFire;
+
+    private int level;
+    private float cooldownPeriod, skillDuration, timer;
+    private bool usingSkill;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    void Update()
+    {
+        if (usingSkill)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+
+    internal void SetLocation(Vector3 origin, Vector3 direction){
+    }
+
+    internal void UseSkill()
+    {
+        switch (UserStatus.Instance.FireLevel)
+        {
+            case 1: Fireball();
+                break;
+            case 2: FireBlast();
+                break;
+            case 3: FireStorm();
+                break;
+            case 4: FireWall();
+                break;
+            case 5: RainOfFire();
+                break;
+            default:
+                break;
+        }
+        usingSkill = true;
+    }
+
+    private void Fireball() 
+    {
+        GameObject fireball;
+    }
+
+    private void FireBlast() { }
+
+    private void FireStorm() { }
+
+    private void FireWall() { }
+
+    private void RainOfFire()
+    {
+
+    }
+}
